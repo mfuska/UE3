0afda1d2920fc9eb1f87372f65fe95ae40ed9700
mfuska <mfuska@gmx.at>
2015-05-17 00:40:37 +0200

    change last error

--- src/OtwayRees/Client/ClientReadInputThread.java
+++ src/OtwayRees/Client/ClientReadInputThread.java
@@ -1,11 +1,9 @@
 package OtwayRees.Client;
 
-import com.sun.deploy.util.StringUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.logging.Logger;
 
 /**
  * Created on 10.05.15.
